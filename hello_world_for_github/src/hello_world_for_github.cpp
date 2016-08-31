644bc9a2821ff5e58ba8b578951d3bfea690c1d3
freddymackay <freddymackay@192.168.0.190>
2016-08-31 23:25:00 +0200

    initial commit
    Signed-off-by: freddymackay <freddymackay@192.168.0.190>

---
+++ hello_world_for_github/src/hello_world_for_github.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : hello_world_for_github.cpp
+// Author      : Freddy_Mackay
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+// new
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
